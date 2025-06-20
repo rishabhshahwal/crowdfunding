55962f57c854f376ef73fd22190748577bdfc754
Rishabh Kumar Shahwal <rishabhshahwal03@gmail.com>
2025-06-20 10:09:17 +0530

    Update crowdfunding.sol

--- contracts/crowdfunding.sol
+++ contracts/crowdfunding.sol
@@ -1,1 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract CrowdFund {
+    address public owner;
+    uint public goalAmount;
+    uint public totalRaised;
+    uint public deadline;
+    mapping(address => uint) public contributions;
+
+    constructor(uint _goalAmount, uint _durationInDays) {
+        owner = msg.sender;
+        goalAmount = _goalAmount;
+        deadline = block.timestamp + (_durationInDays * 1 days);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the contract owner");
+        _;
+    }
+
+    modifier beforeDeadline() {
+        require(block.timestamp <= deadline, "Deadline passed");
+        _;
+    }
+
+    function contribute() public payable beforeDeadline {
+        require(msg.value > 0, "Contribution must be greater than 0");
+        contributions[msg.sender] += msg.value;
+        totalRaised += msg.value;
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function withdraw() public onlyOwner {
+        require(block.timestamp > deadline, "Crowdfunding still active");
+        require(totalRaised >= goalAmount, "Funding goal not met");
+        payable(owner).transfer(address(this).balance);
+    }
+}
 
